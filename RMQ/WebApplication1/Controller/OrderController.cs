b96dbb8f24313be1a77ce6bddc1180a2aac97fa4
saurabhrajankar <saurabhrajankar777@gmail.com>
2023-01-25 19:54:15 +0530

    Implemented NLog

---
+++ RMQ/WebApplication1/Controller/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System;
+using MassTransit;
+using WebApplication1.Model;
+
+namespace WebApplication1.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly IBus _bus;
+        public TicketController(IBus bus)
+        {
+            _bus = bus;
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateTicket(Ticket ticket)
+        {
+            if (ticket != null)
+            {
+                ticket.BookedOn = DateTime.Now;
+                Uri uri = new Uri("rabbitmq://localhost/ticketQueue");
+                var endPoint = await _bus.GetSendEndpoint(uri);
+                await endPoint.Send(ticket);
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ RMQ/WebApplication3/TicketConsumer.cs
@@ -0,0 +1,17 @@
+﻿using MassTransit;
+using System.Threading.Tasks;
+using WebApplication1.Model;
+
+namespace WebApplication3
+{
+    public class TicketConsumer : IConsumer<Ticket>
+    {
+        public async Task Consume(ConsumeContext<Ticket> context)
+        {
+            var data = context.Message;
+            //Validate the Ticket Data
+            //Store to Database
+            //Notify the user via Email / SMS
+        }
+    }
+}
